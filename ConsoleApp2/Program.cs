6f34b369fa0ba84b43c1f3601a4c0a837bd65120
jashanpreet singh <jashanbhullar020@gmail.com>
2019-01-23 11:33:07 -0500

    jashanpreet singh

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -10,6 +10,13 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
+            int sum = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                sum += 1;
+            }
+            Console.WriteLine("sum is{0}", sum);
+            Console.ReadLine();
         }
     }
 }
